f3c6462f982f42016e28226c3e40a326f27866cc
dirk lüsebrink <dirk.luesebrink@gmail.com>
2014-05-26 13:26:50 -0500

    extracted validation to own file, with spec tests

--- web.rb
+++ web.rb
@@ -5,33 +5,12 @@ Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
 $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 require 'ldap-password'
-
-# returns array with error messages or nil
-def validate_password(current, new_pw)
-  checks = [
-    ['new password must differ from current', 
-      lambda {|current, new_pw| current == new_pw}], 
-
-    ['must have 8 or more character', 
-      lambda {|_, new_pw| new_pw.length < 8}],
-
-    ['must contain at least one digit', 
-      lambda {|_, new_pw| new_pw.scan(/\d/).size < 1}], 
-
-    ['must contain at least one uppercase character', 
-      lambda {|_, new_pw| new_pw.scan(/[A-Z]/).size < 1}], 
-
-    ['must contain at least one special ASCII character',
-      lambda {|_, new_pw| new_pw.scan(/[\W]/).size < 1}],
-  ]
-
-  # collect messages for all failing predicates
-  checks.map {|msg, p| p.call(current, new_pw) && msg || nil}.compact
-end
+require 'password-constraints'
 
 # returns array of error strings or nil
 def change_password(username, password, new_pw)
-  errors = validate_password(password, new_pw)
+  #errors = validate_password(password, new_pw)
+  errors = PasswordConstraints::validate(new_pw, password)
   return errors unless errors.empty?
 
   LdapPassword::change_password(username, password, new_pw)
